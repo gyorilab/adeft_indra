49da282da88c6e3619b2b35d6f8be912282f0581
Albert Steppi <albert_steppi@hms.harvard.edu>
2020-02-13 16:29:04 -0500

    Begin work on adding multiprocessing

--- adeft_indra/mine.py
+++ adeft_indra/mine.py
@@ -1,13 +1,12 @@
 import os
 import re
-import uuid
 import json
+import math
+import uuid
 import logging
-from cachetools import LFUCache
-from collections import Counter
-from multiprocessing import Pool
+from cachetools import LRUCache
 from sqlalchemy import text as sqltext
-
+from pathos.multiprocessing import ProcessingPool as Pool
 
 from indra_db.util import get_primary_db
 from indra.literature.adeft_tools import universal_extract_text
@@ -16,10 +15,14 @@ from indra_db.util.content_scripts import get_text_content_from_trids
 from adeft.discover import AdeftMiner, load_adeft_miner, compose
 
 
-class MinerCache(LFUCache):
+class MinerCache(LRUCache):
     def __init__(self, maxsize, location, getsizeof=None):
         super().__init__(maxsize, getsizeof)
         self.location = location
+        try:
+            os.makedirs(location)
+        except FileExistsError:
+            pass
         self.filenames = {}
 
     def popitem(self):
@@ -42,14 +45,12 @@ class MinerCache(LFUCache):
         self[key] = miner
         return miner
 
-    def save_all(self):
+    def dump_all(self):
         with open(os.path.join(self.location,
                                'filenames.json'), 'w') as f:
             json.dump(self.filenames, f)
-        for key, miner in self.items():
-            with open(os.path.join(self.location,
-                                   self.filenames[key]), 'w') as f:
-                miner.dump(f)
+        while self:
+            self.popitem()
 
     def reload(self):
         try:
@@ -63,28 +64,32 @@ def _get_all_trids():
     db = get_primary_db()
     query = 'SELECT id from text_ref'
     res = db.session.execute(sqltext(query))
-    return res.fetchall()
+    return [x[0] for x in res.fetchall()]
 
 
 class MiningOperation(object):
-    def __init__(self, outpath, name, trids, batch_size=1000,
+    def __init__(self, outpath, name, trids, batch_size=10000,
                  pattern=r'(?<=\()\s?\w+(?=\s?\))',
                  shortform_size=(2, 10), filter_function=None,
                  cache_size=1000):
+        self.outpath = outpath
         self.name = name
-        self.miners = MinerCache(cache_size, os.path.join(outpath, name))
-        self.trids = trids
         self.batch_size = batch_size
+        self.cache_size = cache_size
+        self.trids = trids
         self.defining_pattern_pattern = re.compile(pattern)
-        self.banned_prefixes = ('Figure', 'Table', 'Fig')
+        try:
+            os.makedirs(outpath)
+        except FileExistsError:
+            pass
         if filter_function is None:
             def filter_function(shortform):
                 return True if \
-                    shortform_size[0] <= shortform <= shortform_size[1] \
+                    shortform_size[0] <= len(shortform) <= shortform_size[1] \
                     else False
         self.filter_function = filter_function
         logger = logging.getLogger(name)
-        file_handler = logging.fileHandler(os.path.join(outpath, 'mine.log'))
+        file_handler = logging.FileHandler(os.path.join(outpath, 'mine.log'))
         log_format = logging.Formatter('%(asctime)s - %(name)s - '
                                        '%(levelname)s - %(message)s')
         file_handler.setFormatter(log_format)
@@ -108,20 +113,50 @@ class MiningOperation(object):
                        for trid in ref_dict]
             texts = [(trid, universal_extract_text(text)) for
                      trid, text in content if text]
-            yield (i, texts)
+            yield texts
 
     def mine(self):
         texts_stream = self.stream_raw_texts()
-        for batch_number, texts in texts_stream:
+        for batch_number, texts in enumerate(texts_stream):
+            miners = MinerCache(self.cache_size,
+                                os.path.join(self.outpath,
+                                             f'{self.name}_site-'
+                                             '{batch_number}'))
+            self.logger.info(f"Batch: {batch_number}")
             for trid, text in texts:
                 try:
                     shortforms = self.find_shortforms(text)
                     for shortform in shortforms:
-                        miner = self.miners[shortform]
+                        miner = miners[shortform]
                         miner.process_texts([text])
-                except Exception:
-                    self.miners.save_all()
+                except Exception as e:
+                    miners.dump_all()
                     self.logger.error(f'Failure in batch {batch_number}'
                                       f' for text_ref_id {trid} with'
-                                      f' text:\n{text[0:3000]}...')
-            self.miners.save_all()
+                                      f' text:\n{text[0:3000]}...\n'
+                                      f'{e}')
+                    raise e
+            miners.dump_all()
+
+
+def strip_mine_the_world(trids, outpath, n_jobs=1, batch_size=5000,
+                         cache_size=2000):
+    def start_operation(region):
+        trids, region_number = region
+        mining_op = MiningOperation(outpath, f'operation_{region_number}',
+                                    trids,
+                                    batch_size=batch_size,
+                                    cache_size=cache_size)
+        mining_op.mine()
+    operation_size = math.ceil(len(trids)/n_jobs)
+    regions = [(trids[j:j+batch_size], i) for i, j
+               in enumerate(range(0, len(trids), operation_size))]
+    with Pool(n_jobs) as pool:
+        pool.map(start_operation, regions)
+
+
+if __name__ == '__main__':
+    n_jobs = 1
+    all_of_the_content = _get_all_trids()[:10000]
+    strip_mine_the_world(all_of_the_content, '/Users/albertsteppi/msc/mine/',
+                         n_jobs=n_jobs)
