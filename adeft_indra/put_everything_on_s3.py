55e6644ea39f4cf20874a763f40be1297f877f72
Albert Steppi <albert_steppi@hms.harvard.edu>
2020-03-03 16:26:39 -0500

    Update put everything on S3

--- adeft_indra/put_everything_on_s3.py
+++ adeft_indra/put_everything_on_s3.py
@@ -7,30 +7,27 @@ from multiprocessing.pool import ThreadPool
 
 logger = logging.getLogger()
 
+from indra.util import batch_iter
 from indra_db.util import get_primary_db
 
 BUCKET = 'bigmech'
 
 
-def get_all_content_ids():
+def stream_text_content(batch_size=1000000):
     db = get_primary_db()
-    query = 'SELECT id from text_content'
-    res = db.session.execute(text(query))
-    return [x[0] for x in res.fetchall()]
-
-
-def stream_text_content(content_ids, batch_size=1000000):
-    db = get_primary_db()
-    query = """SELECT
-                   text_ref_id, source, text_type, content
-               FROM
-                   text_content
-               WHERE
-                   id in :tcids
+    query_for_ids = 'SELECT id from text_content WHERE id >= 6000000'
+    id_stream = db.session.execute(text(query_for_ids))
+    query_for_content = """SELECT
+                               text_ref_id, source, format, text_type, content
+                           FROM
+                               text_content
+                           WHERE
+                               id in :tcids
             """
-    for i in range(0, len(content_ids), batch_size):
-        tcid_batch = content_ids[i:i+batch_size]
-        res = db.session.execute(text(query), {'tcids': tuple(tcid_batch)})
+    for tcid_batch in batch_iter(id_stream, batch_size):
+        tcids = tuple(tcid[0] for tcid in tcid_batch)
+        logger.info('gathering content')
+        res = db.session.execute(text(query_for_content), {'tcids': tcids})
         yield res
 
 
@@ -42,22 +39,22 @@ def upload_file(x):
     with tempfile.NamedTemporaryFile() as temp:
         with open(temp.name, 'wb') as f:
             f.write(content)
-            client.upload_file(temp.name, BUCKET, key)
+        client.upload_file(temp.name, BUCKET, key)
 
 
 def content_key_generator(batch):
-    for text_ref_id, source, text_type, content in batch:
-        key = os.path.join('content', str(text_ref_id), source, text_type,
-                           f'content.zlib')
+    for text_ref_id, source, format_, text_type, content in batch:
+        key = os.path.join('content', str(text_ref_id), source, format_,
+                           text_type, f'content.zlib')
         content = content.tobytes()
         yield (key, content)
 
 
 def do_it():
-    content_stream = stream_text_content(get_all_content_ids())
+    content_stream = stream_text_content()
     for i, batch in enumerate(content_stream):
-        logger.info(f'working on batch: {i}')
-        with ThreadPool(8) as pool:
+        logger.info(f'uploading content for batch: {i}')
+        with ThreadPool(512) as pool:
             pool.map(upload_file, content_key_generator(batch))
 
 
