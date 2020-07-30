91ee6aec50fc98e1aff185014b643c1d53c82609
steppi <albert_steppi@hms.harvard.edu>
2020-07-30 11:29:08 -0400

    Fix wrong variable used in generate_resources

--- adeft_indra/generate_resources.py
+++ adeft_indra/generate_resources.py
@@ -8,7 +8,7 @@ def download_DF_dictionary():
     client = boto3.client('s3')
     client.download_file(loc.S3_BUCKET_RESOURCES,
                          loc.S3_DOCUMENT_FREQUENCIES_PATH,
-                         loc.S3_DOCUMENT_FREQUENCIES_PATH)
+                         loc.DOCUMENT_FREQUENCIES_PATH)
 
 
 if __name__ == '__main__':
