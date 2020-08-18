540aa389a536ffabeef5f5f5a8176b81419bff80
steppi <albert_steppi@hms.harvard.edu>
2020-08-18 22:20:38 +0000

    Avoid dup when same text found by stmt_id and pmid

--- adeft_indra/content/content.py
+++ adeft_indra/content/content.py
@@ -2,8 +2,9 @@ import sqlite3
 from contextlib import closing
 from multiprocessing import Pool
 from indra.literature.adeft_tools import universal_extract_text
-from indra_db.util.content_scripts import get_text_content_from_pmids, \
-    get_text_content_from_stmt_ids
+from indra_db.util.content_scripts import get_content_identifiers_from_pmids, \
+    get_content_identifiers_from_stmt_ids, _get_text_content
+
 
 from adeft_indra.locations import CACHE_PATH
 
@@ -38,6 +39,19 @@ class ContentCache(object):
                 cur.execute(select_pmids)
                 pmids = cur.fetchall()
         return {str(row[0]) for row in pmids}
+
+    def _load_cached_text_ref_ids(self):
+        with closing(sqlite3.connect(CACHE_PATH)) as conn:
+            select_text_ref_ids = \
+                """SELECT
+                       text_ref_id
+                   FROM
+                       content
+                """
+            with closing(conn.cursor()) as cur:
+                cur.execute(select_text_ref_ids)
+                text_ref_ids = cur.fetchall()
+        return {(row[0]) for row in text_ref_ids}
 
     def _get_text_content_from_stmt_ids_local(self, stmt_ids):
         with closing(sqlite3.connect(CACHE_PATH)) as conn:
@@ -75,6 +89,20 @@ class ContentCache(object):
                 res = cur.execute(query, pmids).fetchall()
         return [row[0] for row in res]
 
+    def _get_text_content_from_text_ref_ids_local(self, text_ref_ids):
+        with closing(sqlite3.connect(CACHE_PATH)) as conn:
+            query = \
+                f"""SELECT DISTINCT
+                        plaintext
+                    FROM
+                        content
+                    WHERE
+                        text_ref_id IN ({','.join(['?']*len(text_ref_ids))})
+                """
+            with closing(conn.cursor()) as cur:
+                res = cur.execute(query, text_ref_ids).fetchall()
+        return [row[0] for row in res]
+
     def _insert_content(self, content_rows):
         with closing(sqlite3.connect(CACHE_PATH)) as conn:
             content_insert_query = """INSERT OR IGNORE INTO
@@ -110,46 +138,80 @@ class ContentCache(object):
 
     def get_text_content_from_stmt_ids(self, stmt_ids, njobs=1):
         output = []
-        precomputed = set(stmt_ids) & self._load_cached_stmt_ids()
-        uncached = list(set(stmt_ids) - precomputed)
-        if precomputed:
+        cached = set(stmt_ids) & self._load_cached_stmt_ids()
+        uncached = list(set(stmt_ids) - cached)
+        if cached:
             output = \
-                self._get_text_content_from_stmt_ids_local(list(precomputed))
+                self._get_text_content_from_stmt_ids_local(list(cached))
         if uncached:
-            ref_dict, content_dict = get_text_content_from_stmt_ids(uncached)
-            content_dict = {ref: xml for ref, xml in content_dict.items()
-                            if xml}
-            plaintexts = extract_plaintext(content_dict.values(), njobs=njobs)
-            stmts_rows = [(stmt_id, ref[0])
-                          for stmt_id, ref in ref_dict.items()]
-            content_rows = [(ref[0], text)
-                            for ref, text in
-                            zip(content_dict.keys(), plaintexts)]
-            output.extend(plaintexts)
-            self._insert_stmts(stmts_rows)
-            self._insert_content(content_rows)
+            identifiers = get_content_identifiers_from_stmt_ids(uncached)
+            text_ref_ids = set(identifier[0] for identifier in identifiers)
+            cached = text_ref_ids & self._load_cached_text_ref_ids()
+            uncached = list(set(text_ref_ids) - cached)
+            stmt_rows = []
+            if cached:
+                output.\
+                    extend(self.
+                           _get_text_content_from_text_ref_ids_local(cached))
+                stmt_rows = [(stmt_id, text_ref_id)
+                             for stmt_id, text_ref_id in identifiers.items()
+                             if text_ref_id in cached]
+            if uncached:
+                identifiers = [idf for idf in identifiers
+                               if idf[0] in cached and idf]
+                ref_dict, content_dict = _get_text_content(identifiers)
+                content_dict = {ref: xml for ref, xml in content_dict.items()
+                                if xml}
+                plaintexts = extract_plaintext(content_dict.values(),
+                                               njobs=njobs)
+                content_rows = [(ref[0], text)
+                                for ref, text in
+                                zip(content_dict.keys(), plaintexts)]
+                stmt_rows = [(stmt_id, ref[0])
+                             for stmt_id, ref in ref_dict.items() if ref]
+                output.extend(plaintexts)
+                self._insert_content(content_rows)
+            if stmt_rows:
+                self._insert_stmts(stmt_rows)
         return output
 
     def get_text_content_from_pmids(self, pmids, njobs=1):
         output = []
-        precomputed = set(pmids) & self._load_cached_pmids()
-        uncached = list(set(pmids) - precomputed)
-        if precomputed:
+        cached = set(pmids) & self._load_cached_pmids()
+        uncached = list(set(pmids) - cached)
+        if cached:
             output = \
-                self._get_text_content_from_pmids_local(list(precomputed))
+                self._get_text_content_from_pmids_local(list(cached))
         if uncached:
-            ref_dict, content_dict = get_text_content_from_pmids(uncached)
-            content_dict = {ref: xml for ref, xml in content_dict.items()
-                            if xml}
-            plaintexts = extract_plaintext(content_dict.values(), njobs=njobs)
-            pmids_rows = [(pmid, ref[0])
-                          for pmid, ref in ref_dict.items()]
-            content_rows = [(ref[0], text)
-                            for ref, text in
-                            zip(content_dict.keys(), plaintexts)]
-            output.extend(plaintexts)
-            self._insert_pmids(pmids_rows)
-            self._insert_content(content_rows)
+            identifiers = get_content_identifiers_from_pmids(uncached)
+            text_ref_ids = set(identifier[0] for identifier in identifiers)
+            cached = text_ref_ids & self._load_cached_text_ref_ids()
+            uncached = list(set(text_ref_ids) - cached)
+            pmid_rows = []
+            if cached:
+                output.\
+                    extend(self.
+                           _get_text_content_from_text_ref_ids_local(cached))
+                pmid_rows = [(pmid, text_ref_id)
+                             for pmid, text_ref_id in identifiers.items()
+                             if text_ref_id in cached]
+            if uncached:
+                identifiers = [idf for idf in identifiers
+                               if idf[0] in cached]
+                ref_dict, content_dict = _get_text_content(identifiers)
+                content_dict = {ref: xml for ref, xml in content_dict.items()
+                                if xml}
+                plaintexts = extract_plaintext(content_dict.values(),
+                                               njobs=njobs)
+                content_rows = [(ref[0], text)
+                                for ref, text in
+                                zip(content_dict.keys(), plaintexts)]
+                pmid_rows = [(pmid, ref[0])
+                             for pmid, ref in ref_dict.items() if ref]
+                output.extend(plaintexts)
+                self._insert_content(content_rows)
+            if pmid_rows:
+                self._insert_stmts(pmid_rows)
         return output
 
     def _setup_tables(self):
