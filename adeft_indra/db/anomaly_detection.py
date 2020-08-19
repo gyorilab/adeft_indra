cef01d32d0989738b4da167b2fee97cf9f454f92
steppi <albert_steppi@hms.harvard.edu>
2020-08-19 19:29:28 +0000

    Add manager to load and save anomaly_detectors to db

--- adeft_indra/db/anomaly_detection.py
+++ adeft_indra/db/anomaly_detection.py
@@ -1,9 +1,79 @@
 import os
 import csv
+import pickle
 import sqlite3
 from contextlib import closing
 
 from adeft_indra.locations import CACHE_PATH
+
+
+class AnomalyDetectorsManager(object):
+    def __init__(self, cache_path=CACHE_PATH):
+        self.cache_path = cache_path
+        self._setup_table()
+
+    def _setup_table(self):
+        make_table_anomaly_detectors = \
+            """CREATE TABLE IF NOT EXISTS anomaly_detectors (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   groundng TEXT,
+                   anomaly_detector BLOB
+            """
+        make_idx_anomaly_detectors_grounding = \
+            """CREATE INDEX IF NOT EXISTS
+                   idx_anomaly_detectors_grounding
+               ON
+                   anomaly_detectors (grounding);
+            """
+        with closing(sqlite3.connect(self.cache_path)) as conn:
+            with closing(conn.cursor()) as cur:
+                for query in [make_table_anomaly_detectors,
+                              make_idx_anomaly_detectors_grounding]:
+                    cur.execute(query)
+            conn.commit()
+
+    def in_table(self, grounding):
+        query = \
+            """SELECT
+                   grounding
+               FROM
+                   anomaly_detectors
+               WHERE
+                   grounding = ?
+               LIMIT 1
+            """
+        with closing(sqlite3.connect(self.cache_path)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query, [grounding])
+                res = cur.fetchone()
+        return len(res) == 1
+
+    def save(self, grounding, anomaly_detector):
+        query = """INSERT INTO
+                       anomaly_detectors (grounding, anomaly_detector)
+                   VALUES
+                       (?, ?);
+                """
+        pdata = pickle.dumps(anomaly_detector, pickle.HIGHEST_PROTOCOL)
+        with closing(sqlite3.connect(self.cache_path)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query, [grounding, sqlite3.Binary(pdata)])
+            conn.commit()
+
+    def load(self, grounding):
+        query = """SELECT
+                       anomaly_detector
+                   FROM
+                       anomaly_detectors
+                   WHERE
+                       grounding = ?
+                   LIMIT 1
+                """
+        with closing(sqlite3.connect(self.cache_path)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query, [grounding])
+                res = cur.fetchone()[0]
+        return pickle.loads(res)
 
 
 class AnomalyDetectionResultsManager(object):
@@ -12,7 +82,7 @@ class AnomalyDetectionResultsManager(object):
         self._setup_table()
 
     def _setup_table(self):
-        make_results_table = \
+        make_table_results = \
             """CREATE TABLE IF NOT EXISTS results (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    agent_text TEXT,
@@ -21,7 +91,7 @@ class AnomalyDetectionResultsManager(object):
                    num_prediction_texts INTEGER,
                    num_anomalous_texts INTEGER,
                    specificity REAL,
-                   std_specifity REAL
+                   std_specifity REAL,
                    UNIQUE(agent_text, grounding)
                );
             """
@@ -33,10 +103,25 @@ class AnomalyDetectionResultsManager(object):
             """
         with closing(sqlite3.connect(self.cache_path)) as conn:
             with closing(conn.cursor()) as cur:
-                for query in [make_results_table,
+                for query in [make_table_results,
                               make_idx_results_agent_text_grounding]:
                     cur.execute(query)
             conn.commit()
+
+    def in_table(self, agent_text, grounding):
+        query = """SELECT
+                       agent_text, grounding
+                   FROM
+                       results
+                   WHERE
+                       agent_text = ? AND grounding = ?
+                   LIMIT 1
+                """
+        with closing(sqlite3.connect(self.cache_path)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query)
+                res = cur.fetchone()
+        return len(res) == 1
 
     def add_row(self, row):
         insert_row = \
