3bf66b0fecc4b4c42d98fd9f4ed9f7c5a8bf07cf
steppi <albert_steppi@hms.harvard.edu>
2020-08-19 17:08:47 +0000

    Add missing imports

--- adeft_indra/db/pmids.py
+++ adeft_indra/db/pmids.py
@@ -1,9 +1,12 @@
 from adeft_indra.mesh import MeshMapper
+from indra.databases.hgnc_client import get_hgnc_name
+from indra.literature.pubmed_client import get_ids_for_mesh, get_ids_for_gene
+
 
 _mesh_mapper = MeshMapper()
 
 
-def get_pmids_for_entity(namespace, id_, major_topic=True)
+def get_pmids_for_entity(namespace, id_, major_topic=True):
     if namespace == 'MESH':
         return get_ids_for_mesh(id_, major_topic=major_topic)
     pmids = set()
