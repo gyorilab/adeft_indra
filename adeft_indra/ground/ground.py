34b76921be57056139ff8e78dfc87e8c549b11e3
steppi <albert_steppi@hms.harvard.edu>
2020-09-11 19:17:05 +0000

    Refactor grounder caching

--- adeft_indra/ground/ground.py
+++ adeft_indra/ground/ground.py
@@ -12,36 +12,13 @@ from adeft_indra.ground.util import expand_dashes, greek_aware_stem, \
     normalize, text_similarity
 
 
-def load_default_index2grounding():
-    index2grounding = {}
-    text2index = defaultdict(list)
-    lexicon = []
-    with open(GROUNDING_TERMS_PATH) as f:
-        reader = csv.reader(f, delimiter='\t')
-        for index, row in enumerate(reader):
-            entry = {'grounding': f'{row[2]}:{row[3]}',
-                     'type': row[5],
-                     'raw_text': row[1],
-                     'name': row[4]}
-            index2grounding[index] = entry
-            text2index[normalize(row[1])].append(index)
-            lexicon.append(row[1])
-    return index2grounding, text2index, lexicon
-
-
 class AdeftGrounder(object):
-    def __init__(self, groundings=None):
-        if groundings is None:
-            index2grounding, text2index, lx = load_default_index2grounding()
-        self.index2grounding = index2grounding
-        self.text2index = text2index
-        self._trie = SearchTrie(lx,
-                                expander=expand_dashes,
-                                token_map=greek_aware_stem)
-        self.type_priority = {'assertion': 0,
-                              'name': 1,
-                              'synonym': 2,
-                              'previous': 3}
+    def __init__(self, rebuild=False):
+        if rebuild or not os.path.exists(GROUNDER_PATH):
+            self._build()
+        else:
+            with open(GROUNDER_PATH, 'rb') as f:
+                self.__dict__.update(pickle.load(f).__dict__)
 
     def ground(self, text):
         results = []
@@ -79,21 +56,32 @@ class AdeftGrounder(object):
                if result['score'][0] > 0]
         return sorted(out, key=lambda x: (-x['score'][0], -x['score'][1]))
 
-
-def load_grounder(rebuild=False):
-    if rebuild or not os.path.exists(GROUNDER_PATH):
-        grounder = AdeftGrounder()
+    def _build(self):
+        index2grounding, text2index, lx = self._load_default_index2grounding()
+        self.index2grounding = index2grounding
+        self.text2index = text2index
+        self._trie = SearchTrie(lx,
+                                expander=expand_dashes,
+                                token_map=greek_aware_stem)
+        self.type_priority = {'assertion': 0,
+                              'name': 1,
+                              'synonym': 2,
+                              'previous': 3}
         with open(GROUNDER_PATH, 'wb') as f:
-            pickle.dump(grounder, f, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        with open(GROUNDER_PATH, 'rb') as f:
-            grounder = pickle.load(f, protocol=pickle.HIGHEST_PROTOCOL)
-    return grounder
+            pickle.dump(self, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-
-def ground(text, grounder=None):
-    if grounder is None:
-        grounder = load_grounder()
-    grounding_results = [entry for entry in grounder.ground(text)
-                         if not entry['partial_match']]
-    return [(entry['grounding'], entry['name']) for entry in grounding_results]
+    def _load_default_index2grounding(self):
+        index2grounding = {}
+        text2index = defaultdict(list)
+        lexicon = []
+        with open(GROUNDING_TERMS_PATH) as f:
+            reader = csv.reader(f, delimiter='\t')
+            for index, row in enumerate(reader):
+                entry = {'grounding': f'{row[2]}:{row[3]}',
+                         'type': row[5],
+                         'raw_text': row[1],
+                         'name': row[4]}
+                index2grounding[index] = entry
+                text2index[normalize(row[1])].append(index)
+                lexicon.append(row[1])
+        return index2grounding, text2index, lexicon
