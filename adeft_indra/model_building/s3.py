ea2a8c7d515a1d5735a39c5eca5cc72caf42c10f
steppi <albert_steppi@hms.harvard.edu>
2020-12-09 04:02:20 +0000

    Fix bug in model name used in s3 upload

--- adeft_indra/model_building/s3.py
+++ adeft_indra/model_building/s3.py
@@ -5,6 +5,7 @@ import tempfile
 
 from adeft.download import get_s3_models
 
+from adeft_indra.model_building.escape import escape_filename
 from adeft_indra.locations import S3_BUCKET_ADEFT, S3_MODELS_PATH
 
 
@@ -14,9 +15,8 @@ def model_to_s3(disambiguator):
     classifier = disambiguator.classifier
 
     shortforms = disambiguator.shortforms
-    model_name = escape_filename(':'.join(sorted(escape_filename(shortform)
-                                                 for shortform in shortforms)))
-
+    model_name = ':'.join(sorted(escape_filename(shortform)
+                                 for shortform in shortforms))
     model_map = {key: model_name for key in grounding_dict}
     s3_models = get_s3_models()
     s3_models.update(model_map)
@@ -46,47 +46,3 @@ def model_to_s3(disambiguator):
         client.upload_file(temp.name, S3_BUCKET_ADEFT,
                            os.path.join(S3_MODELS_PATH, model_name,
                                         f'{model_name}_model.gz'))
-
-
-_escape_map = {'_': '_',
-               '/': 's'}
-
-
-def _escape(char):
-    if char in _escape_map:
-        return '_' + _escape_map[char]
-    if char.islower():
-        return '_' + char.upper()
-    else:
-        return char
-
-
-def escape_filename(filename):
-    """Convert filename for one with escape character before lowercase
-
-    This is done to handle case insensitive file systems. _ is used as an
-    escape character. It is also an escape character for itself.
-    """
-    return ''.join([_escape(char) for char in filename])
-
-
-def unescape_filename(filename):
-    """Inverse of escape_filename"""
-    unescape_map = {value: key for key, value in _escape_map.items()}
-    escape = False
-    output = []
-    for char in filename:
-        if escape:
-            if char in unescape_map:
-                output.append(unescape_map[char])
-            else:
-                output.append(char.lower())
-            escape = False
-        elif char == '_':
-            escape = True
-        elif char in _escape_map:
-            raise ValueError(f'Filename {filename} contains invalid'
-                             ' characters')
-        else:
-            output.append(char)
-    return ''.join(output)
