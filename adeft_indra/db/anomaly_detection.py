874d0741138bb3f3d59089350691f6230079f11d
steppi <albert_steppi@hms.harvard.edu>
2021-01-12 01:56:28 +0000

    Add new results manager
    These bespoke results managers will become obsolete in the near future

--- adeft_indra/db/anomaly_detection.py
+++ adeft_indra/db/anomaly_detection.py
@@ -5,6 +5,7 @@ from contextlib import closing
 from adeft_indra.locations import RESULTS_DB_PATH
 
 
+
 class AnomalyDetectorsManager(object):
     def __init__(self, table_name):
         self.table_name = table_name
@@ -134,6 +135,70 @@ class ResultsManager(object):
                             specificity, std_specificity)
                VALUES
                    (?, ?, ?, ?, ?, ?, ?);
+            """
+        with closing(sqlite3.connect(RESULTS_DB_PATH)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(insert_row, row)
+            conn.commit()
+
+    def get_results(self):
+        query = f"SELECT * FROM {self.table_name}"
+        with closing(sqlite3.connect(RESULTS_DB_PATH)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query)
+                res = cur.fetchall()
+        return res
+
+
+class ADResultsManager(object):
+    def __init__(self, table_name):
+        self.table_name = table_name
+        self._setup_table()
+
+    def _setup_table(self):
+        make_table_results = \
+            f"""CREATE TABLE IF NOT EXISTS {self.table_name} (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   model_name TEXT,
+                   params TEXT,
+                   data TEXT,
+                   UNIQUE(model_name, params)
+               );
+            """
+        make_idx_model_name_params = \
+            f"""CREATE UNIQUE INDEX IF NOT EXISTS
+                   idx_{self.table_name}_model_name_params
+               ON
+                   {self.table_name} (model_name, params);
+            """
+        with closing(sqlite3.connect(RESULTS_DB_PATH)) as conn:
+            with closing(conn.cursor()) as cur:
+                for query in [make_table_results,
+                              make_idx_model_name_params]:
+                    cur.execute(query)
+            conn.commit()
+
+    def in_table(self, model_name, params):
+        query = f"""SELECT
+                       model_name, params
+                   FROM
+                       {self.table_name}
+                   WHERE
+                       model_name = ? AND params = ?
+                   LIMIT 1
+                """
+        with closing(sqlite3.connect(RESULTS_DB_PATH)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute(query, [model_name, params])
+                res = cur.fetchone()
+        return res
+
+    def add_row(self, row):
+        insert_row = \
+            f"""INSERT OR IGNORE INTO
+                    {self.table_name} (model_name, params, data)
+                VALUES
+                    (?, ?, ?);
             """
         with closing(sqlite3.connect(RESULTS_DB_PATH)) as conn:
             with closing(conn.cursor()) as cur:
