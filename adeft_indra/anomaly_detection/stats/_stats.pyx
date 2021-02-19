5519a70b87e9e4dd496b04d699adc9c1ff11a7ae
steppi <albert_steppi@hms.harvard.edu>
2021-02-19 00:13:35 -0500

    Fix confint calculation

--- adeft_indra/anomaly_detection/stats/_stats.pyx
+++ adeft_indra/anomaly_detection/stats/_stats.pyx
@@ -77,8 +77,8 @@ cdef double betainc(float p, float q, float x):
         return D(p, q, x)/p * K(p, q, x)
 
 
-cdef prevalence_cdf(double theta, int n, int t,
-                    double sensitivity, double specificity):
+cdef double prevalence_cdf_exact(double theta, int n, int t,
+                                 double sensitivity, double specificity):
     cdef double c1, c2, numerator, denominator
     c1, c2 = 1 - specificity, sensitivity + specificity - 1
     numerator = (betainc(t+1, n-t+1, c1 + c2*theta) - betainc(t+1, n-t+1, c1))
@@ -90,64 +90,14 @@ cdef prevalence_cdf(double theta, int n, int t,
             return 1.0 if theta == 0.0 else 1.0
     return numerator/denominator
 
-
-ctypedef struct interval_params:
-    int n
-    int t
-    double sens
-    double spec
-    double alpha
-
-
-@cython.cdivision(True)
-cdef double f1(double theta, void *args):
-    cdef interval_params *params = <interval_params *> args
-    return prevalence_cdf(theta, params.n, params.t,
-                          params.sens, params.spec) - params.alpha/2
-
-
-@cython.cdivision(True)
-cdef double f2(double theta, void *args):
-    cdef interval_params *params = <interval_params *> args
-    return prevalence_cdf(theta, params.n, params.t,
-                          params.sens, params.spec) - 1 + params.alpha/2
-
-
-cdef void prevalence_credible_interval_exact(int n, int t,
-                                             double sens, double spec,
-                                             double alpha, double *result):
-    cdef double c1, c2, denominator, left, right
-    cdef interval_params args
-    args.n, args.t = n, t
-    args.sens, args.spec, args.alpha = sens, spec, alpha
-    c1, c2 = 1 - spec, sens + spec - 1
-    denominator = betainc(t+1, n-t+1, c1 + c2) - betainc(t+1, n-t+1, c1)
-    if denominator == 0:
-        if t > n/2:
-            result[0] = 1.0
-            result[1] = 1.0
-            return
-        elif t < n/2:
-            result[0] = 0.0
-            result[1] = 0.0
-            return
-    left = brentq(f1, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
-    right = brentq(f2, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
-    result[0] = left
-    result[1] = right
-
-
-cdef void _prevalence_credible_interval(int n, int t,
-                                        double sens_a, double sens_b,
-                                        double spec_a, double spec_b,
-                                        double alpha, int num_samples,
-                                        double *result):
+cdef double prevalence_cdf(double theta, int n, int t,
+                           double sens_a, double sens_b,
+                           double spec_a, double spec_b,
+                           int num_samples):
     cdef int i
     cdef bitgen_t *rng
-    cdef double temp_result[2]
-    cdef double sens, spec, left_acc, right_acc
-    cdef double _sens_a, _sens_b, _spec_a, _spec_b
     cdef const char *capsule_name = "BitGenerator"
+    cdef double result
     cdef double *sens_array
     cdef double *spec_array
 
@@ -161,24 +111,64 @@ cdef void _prevalence_credible_interval(int n, int t,
     rng = <bitgen_t *> PyCapsule_GetPointer(capsule, capsule_name)
 
     i = 0
-    _sens_a, _sens_b, _spec_a, _spec_b = sens_a, sens_b, spec_a, spec_b
     with x.lock, nogil:
         for i in range(num_samples):
-            sens_array[i] = random_beta(rng, _sens_a, _sens_b)
-            spec_array[i] = random_beta(rng, _spec_a, _spec_b)
+            sens_array[i] = random_beta(rng, sens_a, sens_b)
+            spec_array[i] = random_beta(rng, spec_a, spec_b)
+    result = 0
     i = 0
     for i in range(num_samples):
-        prevalence_credible_interval_exact(n, t,
-                                           sens_array[i],
-                                           spec_array[i],
-                                           alpha, temp_result)
-        result[0] += temp_result[0]
-        result[1] += temp_result[1]
+        result += prevalence_cdf_exact(theta, n, t,
+                                       sens_array[i], spec_array[i])
     PyMem_Free(sens_array)
     PyMem_Free(spec_array)
-    result[0] = result[0]/num_samples
-    result[1] = result[1]/num_samples
+    return result/num_samples
 
+
+ctypedef struct interval_params:
+    int n
+    int t
+    int num_samples
+    double sens_a
+    double sens_b
+    double spec_a
+    double spec_b
+    double alpha
+
+
+@cython.cdivision(True)
+cdef double f1(double theta, void *args):
+    cdef interval_params *params = <interval_params *> args
+    return prevalence_cdf(theta, params.n, params.t,
+                          params.sens_a, params.sens_b,
+                          params.spec_a, params.spec_b,
+                          params.num_samples) - params.alpha/2
+
+
+@cython.cdivision(True)
+cdef double f2(double theta, void *args):
+    cdef interval_params *params = <interval_params *> args
+    return prevalence_cdf(theta, params.n, params.t,
+                          params.sens_a, params.sens_b,
+                          params.spec_a, params.spec_b,
+                          params.num_samples) - 1 + params.alpha/2
+
+cdef void _prevalence_credible_interval(int n, int t,
+                                        double sens_a, double sens_b,
+                                        double spec_a, double spec_b,
+                                        double alpha, int num_samples,
+                                        double *result):
+    cdef double c1, c2, denominator, left, right
+    cdef interval_params args
+    args.n, args.t = n, t
+    args.sens_a, args.sens_b = sens_a, sens_b
+    args.spec_a, args.spec_b = spec_a, spec_b
+    args.alpha = alpha
+    args.num_samples = num_samples
+    left = brentq(f1, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
+    right = brentq(f2, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
+    result[0] = left
+    result[1] = right
 
 def prevalence_credible_interval(n, t, sens_a, sens_b, spec_a, spec_b, alpha,
                                  num_samples=5000):
