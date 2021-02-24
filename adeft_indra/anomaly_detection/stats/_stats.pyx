aad4c34b01703bd38ac5cd22e8d67e3c26522558
steppi <albert_steppi@hms.harvard.edu>
2021-02-24 00:55:15 -0500

    Clear out now unused functions

--- adeft_indra/anomaly_detection/stats/_stats.pyx
+++ adeft_indra/anomaly_detection/stats/_stats.pyx
@@ -76,24 +76,18 @@ cdef double betainc(float p, float q, float x):
     else:
         return D(p, q, x)/p * K(p, q, x)
 
+def py_betainc(a, b, x):
+    return betainc(a, b, x)
 
-cdef double prevalence_cdf_exact(double theta, int n, int t,
-                                 double sensitivity, double specificity):
-    cdef double c1, c2, numerator, denominator
-    c1, c2 = 1 - specificity, sensitivity + specificity - 1
-    numerator = (betainc(t+1, n-t+1, c1 + c2*theta) - betainc(t+1, n-t+1, c1))
-    denominator = betainc(t+1, n-t+1, c1 + c2) - betainc(t+1, n-t+1, c1)
-    if denominator == 0:
-        if t > n/2:
-            return 1.0 if theta == 1.0 else 0.0
-        elif t < n/2:
-            return 1.0 if theta == 0.0 else 1.0
-    return numerator/denominator
 
-cdef double prevalence_cdf(double theta, int n, int t,
-                           double sens_a, double sens_b,
-                           double spec_a, double spec_b,
-                           int num_samples):
+ctypedef double (*prevalence_function)(double, int, int, double, double)
+
+
+cdef double beta_sample(double theta, int n, int t,
+                        double sens_a, double sens_b,
+                        double spec_a, double spec_b,
+                        prevalence_function func,
+                        int num_samples):
     cdef int i
     cdef bitgen_t *rng
     cdef const char *capsule_name = "BitGenerator"
@@ -118,65 +112,30 @@ cdef double prevalence_cdf(double theta, int n, int t,
     result = 0
     i = 0
     for i in range(num_samples):
-        result += prevalence_cdf_exact(theta, n, t,
-                                       sens_array[i], spec_array[i])
+        result += func(theta, n, t, sens_array[i], spec_array[i])
     PyMem_Free(sens_array)
     PyMem_Free(spec_array)
     return result/num_samples
 
 
-ctypedef struct interval_params:
-    int n
-    int t
-    int num_samples
-    double sens_a
-    double sens_b
-    double spec_a
-    double spec_b
-    double alpha
+cdef double prevalence_cdf_known_sens_spec(double theta, int n, int t,
+                                           double sensitivity,
+                                           double specificity):
+    cdef double c1, c2, numerator, denominator
+    c1, c2 = 1 - specificity, sensitivity + specificity - 1
+    numerator = (betainc(t+1, n-t+1, c1 + c2*theta) - betainc(t+1, n-t+1, c1))
+    denominator = betainc(t+1, n-t+1, c1 + c2) - betainc(t+1, n-t+1, c1)
+    if denominator == 0:
+        return theta
+    return numerator/denominator
 
 
-@cython.cdivision(True)
-cdef double f1(double theta, void *args):
-    cdef interval_params *params = <interval_params *> args
-    return prevalence_cdf(theta, params.n, params.t,
-                          params.sens_a, params.sens_b,
-                          params.spec_a, params.spec_b,
-                          params.num_samples) - params.alpha/2
+cdef double _prevalence_cdf(double theta, int n, int t, double sens_a,
+                            double sens_b, double spec_a, double spec_b,
+                            int num_samples):
+    return beta_sample(theta, n, t, sens_a, sens_b, spec_a, spec_b,
+                       prevalence_cdf, num_samples)
 
 
-@cython.cdivision(True)
-cdef double f2(double theta, void *args):
-    cdef interval_params *params = <interval_params *> args
-    return prevalence_cdf(theta, params.n, params.t,
-                          params.sens_a, params.sens_b,
-                          params.spec_a, params.spec_b,
-                          params.num_samples) - 1 + params.alpha/2
-
-cdef void _prevalence_credible_interval(int n, int t,
-                                        double sens_a, double sens_b,
-                                        double spec_a, double spec_b,
-                                        double alpha, int num_samples,
-                                        double *result):
-    cdef double c1, c2, denominator, left, right
-    cdef interval_params args
-    args.n, args.t = n, t
-    args.sens_a, args.sens_b = sens_a, sens_b
-    args.spec_a, args.spec_b = spec_a, spec_b
-    args.alpha = alpha
-    args.num_samples = num_samples
-    left = brentq(f1, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
-    right = brentq(f2, 0, 1, &args, 1e-3, 1e-3, 100, NULL)
-    result[0] = left
-    result[1] = right
-
-def prevalence_credible_interval(n, t, sens_a, sens_b, spec_a, spec_b, alpha,
-                                 num_samples=5000):
-    cdef double result[2]
-    cdef int _n, _t, _num_samples
-    cdef double _sens_a, _sens_b, _spec_a, _spec_b, _alpha
-    _n, _t, _num_samples, _alpha = n, t, num_samples, alpha
-    _sens_a, _sens_b, _spec_a, _spec_b, = sens_a, sens_b, spec_a, spec_b
-    _prevalence_credible_interval(_n, _t, _sens_a, _sens_b, _spec_a, _spec_b,
-                                  _alpha, _num_samples, result)
-    return (result[0], result[1])
+def prevalence_cdf(theta, int, t, sens_a, sens_b, spec_a, spec_b,
+                   num_samples=10000)
