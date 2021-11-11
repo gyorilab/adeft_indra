5124868c4f62bfff7db35d01fe320c229e65bb9d
Ubuntu <ubuntu@ip-172-31-21-239.ec2.internal>
2021-11-11 19:10:42 +0000

    Make neg set script generate sample and full set

--- adeft_indra/anomaly_detection/negative_set.py
+++ adeft_indra/anomaly_detection/negative_set.py
@@ -5,7 +5,9 @@ import subprocess
 
 from gensim.corpora import Dictionary
 
-from indra_db_lite import get_text_sample
+
+from indra_db_lite import get_plaintexts_for_text_ref_ids
+from indra_db_lite import get_text_ref_ids_for_pmids
 
 from opaque.nlp.featurize import BaselineTfidfVectorizer
 
@@ -31,13 +33,20 @@ class TextFuzzer:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('inpath')
     parser.add_argument('outpath')
     parser.add_argument('n_threads')
     args = parser.parse_args()
+    inpath = args.inpath
     outpath = args.outpath
     n_threads = args.n_threads
-    content = get_text_sample(100000, text_types=['fulltext'])
-    content.process()
+    with open(inpath) as f:
+        pmids = json.load(f)
+    pmids = [int(pmid) for pmid in pmids]
+    trids = get_text_ref_ids_for_pmids(pmids).values()
+    content = get_plaintexts_for_text_ref_ids(
+        trids, text_types=['fulltext']
+    )
     fuzz_text = TextFuzzer()
     result = {
         trid: fuzz_text(text)
@@ -47,8 +56,13 @@ if __name__ == '__main__':
         trid: text for trid, text in result.items() if text is not None
         and len(text) > 500
     }
+    gen = Random(1729)
+    sample = set(gen.sample(list(result.keys()), k=50000))
+    sample_texts = {
+        trid: text for trid: text in result.items()
+        if trid in sample
+    }
     with open(outpath, 'w') as f:
+        json.dump(sample_texts, f, indent=True)
+    with open(outpath + '.full', 'w') as f:
         json.dump(result, f, indent=True)
-    subprocess.run(
-        ["xz", "-v", "-1", n_threads, outpath]
-    )
