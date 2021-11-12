d2c6e80a1ae7291dc560fcb518dce82bf75ee3e4
Ubuntu <ubuntu@ip-172-31-19-112.ec2.internal>
2021-11-12 22:15:54 +0000

    Remove obsolete locations

--- adeft_indra/locations.py
+++ adeft_indra/locations.py
@@ -9,22 +9,11 @@ here = os.path.dirname(os.path.abspath(__file__))
 S3_BUCKET_ADEFT = 'adeft'
 # Path to Models folder in bucket
 S3_MODELS_PATH = os.path.join(__version__, 'Models')
-# S3 Bucket for resource files
-S3_BUCKET_RESOURCES = 'bigmech'
-# Path to document frequency dictionary in bucket
-S3_DOCUMENT_FREQUENCIES_PATH = 'entrez_pubmed_dictionary.pkl'
 
 
 ADEFT_INDRA_HOME = os.environ.get('ADEFT_INDRA_HOME') or \
     os.path.expanduser('~/.adeft_indra')
 
-
-CONTENT_DB_PATH = os.path.join(ADEFT_INDRA_HOME, 'content.db')
-# A temporary expedient during migration
-UPDATE_DB_PATH = os.path.join(ADEFT_INDRA_HOME, 'update_text_content.db')
-RESULTS_DB_PATH = os.path.join(ADEFT_INDRA_HOME, 'results.db')
-DOCUMENT_FREQUENCIES_PATH = os.path.join(ADEFT_INDRA_HOME,
-                                         'pubmed_dictionary.pkl')
 GROUNDING_THESAURUS_PATH = os.path.join(ADEFT_INDRA_HOME,
                                         'grounding_thesaurus.json')
 GROUNDER_PATH = os.path.join(ADEFT_INDRA_HOME, 'adeft_grounder.pkl')
