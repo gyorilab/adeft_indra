e8da0b754f735411dd5c5e194ddcf5409cd50eb4
Ubuntu <ubuntu@ip-172-31-28-166.ec2.internal>
2021-12-28 21:00:14 +0000

    Remove obsolote extensions from setup

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-import sys
-import numpy as np
 from os import path
-from setuptools.extension import Extension
-from setuptools import dist, setup, find_packages, Command
+from setuptools import setup, find_packages
 
 
 here = path.abspath(path.dirname(__file__))
@@ -10,36 +7,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
-if '--use-cython' in sys.argv:
-    USE_CYTHON = True
-    sys.argv.remove('--use-cython')
-else:
-    USE_CYTHON = False
-
-ext = '.pyx' if USE_CYTHON else '.c'
-
-defs = [('NPY_NO_DEPRECATED_API', 0)]
-inc_path = np.get_include()
-lib_path = path.join(inc_path, '..', '..', 'random', 'lib')
-
-extensions = [
-    Extension('adeft_indra.anomaly_detection.stats._stats',
-              ['adeft_indra/anomaly_detection/stats/_stats' + ext],
-              include_dirs=[inc_path],
-              library_dirs=[lib_path],
-              libraries=['npyrandom'],
-              define_macros=defs),
-    Extension('adeft_indra.anomaly_detection._tree_kernel',
-              ['adeft_indra/anomaly_detection/_tree_kernel' + ext],
-              include_dirs=[inc_path],
-              define_macros=defs)
-    ]
-
-if USE_CYTHON:
-    from Cython.Build import cythonize
-    extensions = cythonize(extensions,
-                           compiler_directives={'language_level': 3})
 
 setup(name='adeft_indra',
       version='0.0.0',
@@ -53,5 +20,4 @@ setup(name='adeft_indra',
           'Programming Language :: Python :: 3.8'
       ],
       packages=find_packages(),
-      ext_modules=extensions,
       include_package_data=True)
